a26852938c5bd067286a968b2c107550a58103fb
Salama Nour El Dean <salamanoureldean2001@gmail.com>
2025-01-28 14:37:37 -0500

    Added box methods for putting and getting messages.

--- src/Box.java
+++ src/Box.java
@@ -1,2 +1,52 @@
 public class Box {
+    // Boolean to hold box state
+    private boolean boxEmpty = true;
+    // Message to be passed to the box
+    private EventMessage message;
+
+    /**
+     * Place message in the box for it to be read
+     * @param message
+     * @throws InterruptedException
+     */
+    public synchronized void put(EventMessage message) throws InterruptedException {
+        // Wait while the box is empty
+        while (!boxEmpty) {
+            wait();
+        }
+        // Put message in the box
+        this.message = message;
+
+        // Box is not empty anymore so flip bool
+        boxEmpty = false;
+
+        // Notify other threads that the box has a message
+        notifyAll();
+    }
+
+    /**
+     * Method to get a message from the box
+     * @return The message placed in the box
+     * @throws InterruptedException
+     */
+    public synchronized EventMessage get() throws InterruptedException {
+        // Wait while box is empty
+        while (boxEmpty) {
+            wait();
+        }
+
+        // Variable to retrieve and return message from the box
+        EventMessage tmpMsg = message;
+
+        // Set message as null again since this box no longer has a message
+        message = null;
+
+        // Flip boolean to true since box is now empty
+        boxEmpty = true;
+
+        // Notify waiting threads that box is now empty
+        notifyAll();
+
+        return tmpMsg;
+    }
 }
