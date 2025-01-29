edd0ca28719763a3c48c55d1a090238b171854c7
Hasib Khodayar <khodayarhasib@gmail.com>
2025-01-28 20:17:41 -0500

    Changed IncidentEvent attributes to private and added getters.
    **Note this was written by Hasib Khodayar and Hajar Assim on live codeshare and committed on a singular laptop

--- src/events/IncidentEvent.java
+++ src/events/IncidentEvent.java
@@ -3,12 +3,12 @@ package events;
 import java.util.AbstractMap;
 
 public class IncidentEvent {
-    String timestamp;
-    int zoneId;
-    EventType eventType;
-    Severity severity;
-    AbstractMap.SimpleEntry<Integer, Integer> startCoordinates;
-    AbstractMap.SimpleEntry<Integer, Integer> endCoordinates;
+    private String timestamp;
+    private int zoneId;
+    private EventType eventType;
+    private Severity severity;
+    private AbstractMap.SimpleEntry<Integer, Integer> startCoordinates;
+    private AbstractMap.SimpleEntry<Integer, Integer> endCoordinates;
 
     public IncidentEvent(String timestamp, int zoneId, String eventType, String severity, String startCoords, String endCoords) {
         this.timestamp = timestamp;
@@ -17,6 +17,30 @@ public class IncidentEvent {
         this.severity = Severity.fromString(severity);
         this.startCoordinates = parseCoordinates(startCoords);
         this.endCoordinates = parseCoordinates(endCoords);
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public int getZoneId() {
+        return zoneId;
+    }
+
+    public EventType getEventType() {
+        return eventType;
+    }
+
+    public Severity getSeverity() {
+        return severity;
+    }
+
+    public AbstractMap.SimpleEntry<Integer, Integer> getStartCoordinates() {
+        return startCoordinates;
+    }
+
+    public AbstractMap.SimpleEntry<Integer, Integer> getEndCoordinates() {
+        return endCoordinates;
     }
 
     private AbstractMap.SimpleEntry<Integer, Integer> parseCoordinates(String coords) {
