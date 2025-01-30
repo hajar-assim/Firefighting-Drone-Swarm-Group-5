00576b2ba143bb0554ee275366e9c62bcb6208d5
Colin Chen <colin1843@gmail.com>
2025-01-30 00:35:11 -0500

    Implement multibox functionality

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,24 +3,33 @@ package main;
 import subsystems.FireIncidentSubsystem;
 import subsystems.DroneSubsystem;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("\n === Firefighting Drone System Starting... ===\n");
 
-        // create and start subsystem threads
-        EventQueueManager eventQueueManager = new EventQueueManager();
-        Thread fireIncidentThread = new Thread(new FireIncidentSubsystem(eventQueueManager));
-        //Thread schedulerThread = new Thread(new Scheduler());
-        //Thread droneThread = new Thread(new DroneSubsystem());
+        // create and start fire incident subsystem thread
+        EventQueueManager fireIncidentManager = new EventQueueManager();
+        Thread fireIncidentThread = new Thread(new FireIncidentSubsystem(fireIncidentManager));
+
+        // create and start drone subsystem thread
+        ArrayList<EventQueueManager> droneManagers = new ArrayList<EventQueueManager>();
+        EventQueueManager droneManager1 = new EventQueueManager();
+        droneManagers.add(droneManager1);
+        //Thread droneThread1 = new Thread(new DroneSubsystem(droneManager1));
+
+        // create and start scheduler thread
+        Thread schedulerThread = new Thread(new Scheduler(fireIncidentManager, droneManagers));
 
         fireIncidentThread.start();
-        //schedulerThread.start();
+        schedulerThread.start();
         //droneThread.start();
 
         try {
             fireIncidentThread.join();
-            //schedulerThread.join();
+            schedulerThread.join();
             //droneThread.join();
         } catch (InterruptedException e) {
             System.err.println("Main thread interrupted: " + e.getMessage());
--- src/main/Scheduler.java
+++ src/main/Scheduler.java
@@ -2,25 +2,38 @@ package main;
 
 import events.IncidentEvent;
 
+import java.util.ArrayList;
+
 public class Scheduler implements Runnable{
-    private EventQueueManager eventManager;
-    public Scheduler(EventQueueManager eventManager){
-        this.eventManager = eventManager;
+    private EventQueueManager fireIncidentManager;
+    private ArrayList<EventQueueManager> droneManagers;
+
+    public Scheduler(EventQueueManager fireIncidentManager, ArrayList<EventQueueManager> droneManagers){
+        this.fireIncidentManager = fireIncidentManager;
+        this.droneManagers = droneManagers;
     }
+
+    private EventQueueManager scheduleDrone(){
+        // Placeholder for while only one drone is being considered
+        return droneManagers.get(0);
+    }
+
     public void run(){
         while(true){
-            IncidentEvent request = eventManager.get("Scheduler");
+            IncidentEvent request = fireIncidentManager.get("Scheduler");
             System.out.println("Received request from Fire Incident Subsystem");
 
-            request.setReceiver("FireIncidentSubsystem");
-            eventManager.put(request);
-            System.out.println("Forwarding request to Drone Subsystem");
-
-            IncidentEvent response = eventManager.get("Scheduler");
-            System.out.println("Received response from Drone Subsystem");
+            EventQueueManager droneManager = scheduleDrone();
 
             request.setReceiver("DroneSubsystem");
-            eventManager.put(response);
+            droneManager.put(request);
+            System.out.println("Forwarding request to Drone Subsystem");
+
+            IncidentEvent response = droneManager.get("Scheduler");
+            System.out.println("Received response from Drone Subsystem");
+
+            request.setReceiver("FireIncidentSubsystem");
+            fireIncidentManager.put(response);
             System.out.println("Forwarding response to Fire Incident Subsystem");
         }
     }
