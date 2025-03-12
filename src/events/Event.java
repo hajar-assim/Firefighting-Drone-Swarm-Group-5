6c31ff755d965468f806e6d47136fc24fb33295a
Colin Chen <colin1843@gmail.com>
2025-03-11 23:27:26 -0400

    Networking for fire subsystem

--- src/events/Event.java
+++ src/events/Event.java
@@ -1,10 +1,12 @@
 package events;
 
+import java.io.Serializable;
+
 /**
  * Represents an abstract event with a timestamp.
  * Subclasses must implement methods to convert the event to and from a string representation.
  */
-public abstract class Event {
+public abstract class Event implements Serializable {
     private String timeStamp;
 
     /**
